1244224a76bf4cec68103260df31135e9e27f74d
David Palomino <238708030+DavidPalo@users.noreply.github.com>
2025-12-14 20:30:33 +0100

    Lab Python: realizado ejercicio extra

---
+++ ejecutar_ejercicio_extra_slurm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH -p hpc-bio-mendel # partition (queue)
+#SBATCH --chdir=/home/alumno16/lab_4/ # working directory
+#SBATCH -J lanzar_jupyter_extra_de_SLURM
+
+module load anaconda/2025.06
+
+ipython pi-solution-alumno16.ipynb $1
+
+module unload anaconda/2025.06
+
